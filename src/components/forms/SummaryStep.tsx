a43efd5fa4ac1e900fc251d9decc8a3a92e4460c
philouLeF <delcroixphilippe73@gmail.com>
2025-08-09 23:58:05 +0200

    logique de l'étape summary finie

--- src/components/forms/SummaryStep.tsx
+++ src/components/forms/SummaryStep.tsx
@@ -3,11 +3,27 @@ import type { FormData } from "../../lib/schemas";
 import { StepDescription } from "../elements/StepDescription";
 import { Title } from "../elements/Title";
 import { Card, CardContent } from "../ui/card";
+import { ADDONS_DATA, PLANS_DATA } from "../../lib/constants";
 
 export const SummaryStep = ({ form }: { form: UseFormReturn<FormData> }) => {
   const { watch } = form;
   const { plan, isYearlyPlanSelected, addons } = watch();
-  console.log(plan, isYearlyPlanSelected, addons);
+
+  const planPrice = isYearlyPlanSelected
+    ? PLANS_DATA[plan].yearlyPrice
+    : PLANS_DATA[plan].monthlyPrice;
+
+  const selectedAddons = Object.entries(addons)
+    .filter(([, value]) => value === true)
+    .map(([key]) => ADDONS_DATA[key as keyof typeof ADDONS_DATA]);
+
+  const totalPrice =
+    planPrice +
+    selectedAddons.reduce((acc, addon) => {
+      return (
+        acc + (isYearlyPlanSelected ? addon.yearlyPrice : addon.monthlyPrice)
+      );
+    }, 0);
 
   return (
     <div>
@@ -16,21 +32,39 @@ export const SummaryStep = ({ form }: { form: UseFormReturn<FormData> }) => {
       <Card>
         <CardContent>
           <div>
-            <div>
+            <div className="flex justify-between items-center">
               <div>
                 <p>
                   {plan} ({isYearlyPlanSelected ? "Yearly" : "Monthly"})
                 </p>
                 <button>Change</button>
               </div>
-              <p></p>
+              <p>${planPrice}</p>
             </div>
             <hr />
-            <div></div>
-            <div></div>
+            {selectedAddons.map((addon) => {
+              return (
+                <div className="flex justify-between">
+                  <p>{addon.name}</p>
+                  <p>
+                    +$
+                    {isYearlyPlanSelected
+                      ? `${addon.yearlyPrice}/yr`
+                      : `${addon.monthlyPrice}/mo`}
+                  </p>
+                </div>
+              );
+            })}
           </div>
         </CardContent>
       </Card>
+      <div className="flex justify-between items-center">
+        <p>Total (per {isYearlyPlanSelected ? "year" : "month"})</p>
+        <div>
+          ${totalPrice}
+          {isYearlyPlanSelected ? "/yr" : "/mo"}
+        </div>
+      </div>
     </div>
   );
 };
