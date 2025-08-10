fcab00f48d9d130b68e73ee56827f121f5aea693
philouLeF <delcroixphilippe73@gmail.com>
2025-08-10 20:19:12 +0200

    correction base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/multi-step-form/',
+  base: '/multi-step-form-main/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
